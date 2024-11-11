975e6762ce05fd64e09927835f7dcf02598fbae4
Jessica Ye <ssicaj00@gmail.com>
2024-11-11 00:39:27 -0500

    added blueprints to backend

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,36 @@
 from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import flash, redirect, render_template, request, session, abort
 import os
+from routes import blueprints
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-   if not session.get('logged_in'):
-      return render_template('login.html')
-   else:
-      return "Hello Boss!"
+for bp in blueprints:
+    app.register_blueprint(bp)
 
-@app.route('/login', methods=['POST'])
-def do_admin_login():
-   if request.form['password'] == 'password' and request.form['username'] == 'admin':
-      session['logged_in'] = True
-   else:
-      flash('wrong password!')
-   return home()
+@app.route('/')
+def index():
+    # This renders an HTML file that loads the Next.js app.
+    return render_template('index.html')
+
+# @app.route('/api/data')
+# def get_data():
+#     return {"message": "Hello from Flask!"}
+
+
+# def home():
+#    if not session.get('logged_in'):
+#       return render_template('login.html')
+#    else:
+#       return "Hello Boss!"
+
+# @app.route('/login', methods=['POST'])
+# def do_admin_login():
+#    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+#       session['logged_in'] = True
+#    else:
+#       flash('wrong password!')
+#    return home()
 
 if __name__ == "__main__":
    app.secret_key = os.urandom(12)
---
+++ backend/routes/__init__.py
@@ -0,0 +1,4 @@
+from .users import user_bp
+
+# List of all blueprints
+blueprints = [user_bp]
