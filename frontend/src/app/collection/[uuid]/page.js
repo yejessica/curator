3fd16afd4299746ea4b404f075effcee116e5687
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-11-19 16:42:23 -0500

    Added arbitrary editing to collections, can add or remove or modify any exhibits

--- frontend/src/app/collection/[uuid]/page.js
+++ frontend/src/app/collection/[uuid]/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Navbar from '../../../components/navbar';
+import Navbar from '../../../components/Navbar';
 import Image from 'next/image';
 
 export default function Collection({ params: paramsPromise }) {
