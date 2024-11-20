b8a15a4a4b23be58e8bae9a08d63ea4a5ca43b78
Jessica Ye <ssicaj00@gmail.com>
2024-11-20 15:27:53 -0500

    added ssl certificate

--- backend/app.py
+++ backend/app.py
@@ -1108,4 +1108,4 @@ def get_collection_id_from_url(url):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context="adhoc")
