6ea6342304b0167da82a5b5eb04868ef38a56e9a
Joseph Jojoe <28976447+josephjojoe@users.noreply.github.com>
2024-11-20 19:59:54 -0500

    added session dict

--- backend/app.py
+++ backend/app.py
@@ -69,7 +69,7 @@ def verify_password(password: str, hashed: str) -> bool:
 @app.before_request
 def before_request():
     try:
-        print("Session contents:", session)
+        print("Session before request:", dict(session))
         g.conn = engine.connect()
         # Print the logged-in user's email from the session, if it exists
         if 'email' in session:
@@ -77,7 +77,7 @@ def before_request():
         else:
             print("No user is currently logged in.")
     except:
-        print("Session after request:", session)
+        print("Session after request:", dict(session))
         print("Uh oh, problem connecting to the database")
         import traceback
         traceback.print_exc()
