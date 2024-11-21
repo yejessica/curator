66b2b1f5248d93b0d492bfe8eaddbc1df9b14785
Jessica Ye <ssicaj00@gmail.com>
2024-11-20 19:01:54 -0500

    changed cors config origins

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,8 @@ import urllib.parse
 
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
-# CORS(app, origins=["https://curator-smoky.vercel.app"], supports_credentials=True)
+# CORS(app, supports_credentials=True)
+CORS(app, origins=["https://curator-smoky.vercel.app"], supports_credentials=True)
 
 
 # Use the provided secret key
